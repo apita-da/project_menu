6955fdc99f3082bf6880e872360d15782a760a0f
Amalia Pita da Veiga <amalia.pita@the-cocktail.com>
2024-12-24 00:09:50 +0100

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-
+// @ts-ignore
+const baseUrl = import.meta.env.VITE_BASE_URL || '/project_menu/';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/project_menu/',
+  base: baseUrl,
   plugins: [
     vue(),
     vueDevTools(),    
