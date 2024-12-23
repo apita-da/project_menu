156676b15652415e3aa923a17b422264ae1fbf67
Amalia Pita da Veiga <amalia.pita@the-cocktail.com>
2024-12-23 20:57:34 +0100

    try deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/project_menu/',
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools(),    
+    viteStaticCopy({
+      targets: [
+        { src: 'CNAME', dest: '.' } // Copia el archivo CNAME al directorio dist
+      ]
+    })
   ],
   resolve: {
     alias: {
