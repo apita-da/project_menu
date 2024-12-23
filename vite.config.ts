a25bebd7b39218e3b37cf292bb1b6fe3e45b2023
Amalia Pita da Veiga <amalia.pita@the-cocktail.com>
2024-12-23 23:53:32 +0100

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: isGitHubPages ? '/' : '/project_menu/',
+  base: '/project_menu/',
   plugins: [
     vue(),
     vueDevTools(),    
