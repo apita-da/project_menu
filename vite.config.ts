51ac7d335772f0c1bec4c58b5f5fb4cd61289eff
Amalia Pita da Veiga <amalia.pita@the-cocktail.com>
2024-12-24 00:16:16 +0100

    run netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,35 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-// @ts-ignore
-const baseUrl = import.meta.env.VITE_BASE_URL || '/project_menu/';
-// https://vite.dev/config/
-export default defineConfig({
-  base: baseUrl,
-  plugins: [
-    vue(),
-    vueDevTools(),    
 
-  ],
-  build: {
-    outDir: 'docs',
-    modulePreload: false,
-    target: 'esnext',
-    minify: true,
-    cssCodeSplit: false,
-    rollupOptions: {
-      output: {
-        minifyInternalExports: true,
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const baseUrl = mode === 'netlify' ? '/' : '/project_menu/';
+  
+  return {
+    base: baseUrl,
+    plugins: [
+      vue(),
+      vueDevTools(),    
+    ],
+    build: {
+      outDir: 'docs',
+      modulePreload: false,
+      target: 'esnext',
+      minify: true,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          minifyInternalExports: true,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      },
     },
-  },
-})
+  }
+  })
+  
