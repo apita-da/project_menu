5f4ae2667d0d4be7693ed81253a587da2663542f
Amalia Pita da Veiga <amalia.pita@the-cocktail.com>
2024-12-23 22:01:03 +0100

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/project_menu/',
+  base: '/project_menu/dist',
   plugins: [
     vue(),
     vueDevTools(),    
@@ -14,7 +14,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+
     },
   },
 })
